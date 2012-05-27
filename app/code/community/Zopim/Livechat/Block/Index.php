08b49b1212b9d0034158142c3aa53cd0ce32a184
Nhan Nguyen <ntnhan220488@yahoo.com>
2012-05-27 19:23:17 +0700

    Remove Zopim.

--- app/code/community/Zopim/Livechat/Block/Index.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Zopim_Livechat_Block_Index extends Mage_Core_Block_Template
-{
-	protected function _toHtml()
-	{
-		return 'Zopim LiveChat installed ok';
-	}
-}
--- app/code/community/Zopim/Livechat/Helper/Data.php
+++
@@ -1,4 +0,0 @@
-<?php
-class Zopim_Livechat_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
